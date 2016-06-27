56ffa4d4f42349a91d5d0fa636dd0efb22536c97
rizakii <riza.alaudin@kii.com>
2016-06-27 14:45:31 +0900

    remove unsused files

--- model/Color.swift
+++
@@ -1,9 +0,0 @@
-//TODO: implementation
-private let _actionName = "SetColor"
-struct SetColorAction : NamedAction{
-    var color : ColorRGB
-    func actionName() -> String {return _actionName}
-}
-struct SetColorActionResult : NamedAction{
-    func actionName() -> String {return _actionName}
-}
